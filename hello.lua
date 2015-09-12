73b6c67f15f34c103c571fe2ec2d5b64caa6b539
c475 <danjamesbond@gmail.com>
2015-09-12 14:01:24 -0700

    .

--- hello.lua
+++ hello.lua
@@ -1,3 +1,17 @@
+local r = require("resty.redis")
+local redis = r:new()
+
+-- 1 second timeout
+redis.timeout(1000)
+
+local ok, error = redis:connect("127.0.0.1", 6379)
+
+if not ok then
+    ngx.say("failed to connect to redis server: ", error)
+    return
+end
+
+
 local ret = {}
 
 ngx.req.get_body_data()
@@ -7,7 +21,13 @@ ret.get = ngx.req.get_uri_args()
 ret.post = ngx.req.get_post_args()
 ret.file = ngx.req.get_body_file()
 
-redis:set(ngx.var.remote_addr, json.encode(ret))
+local ok, error = redis:set(ngx.var.remote_addr, json.encode(ret))
+
+if not ok then
+    ngx.say("unable to set in redis: ", error)
+    return
+end
+
 
 ngx.say([[
 
@@ -21,6 +41,7 @@ ngx.say([[
 
     <body>
         <h1>This is my new website</h1>
+        <p>]] .. json.encode(ret) .. [[</p>
     </body>
 
     </html>
