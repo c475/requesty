7ba27cd866acc612339a556e3432993e49eb93ab
c475 <danjamesbond@gmail.com>
2015-09-12 14:16:38 -0700

    .

--- hello.lua
+++ hello.lua
@@ -10,14 +10,44 @@ if not ok then
 end
 
 
+local lib = {}
 local ret = {}
+
+function try(f)
+    local status, exception = pcall(f)
+
+    if not status then
+        return nil
+    else
+        return f()
+    end
+end
+
+
+function lib.get_headers ()
+    return ngx.req.get_headers()
+end
+
+function lib.get_uri ()
+    return ngx.req.get_uri_args()
+end
+
+function lib.get_body ()
+    return ngx.req.get_post_args
+end
+
+function lib.get_file ()
+    return ngx.req.get_body_file()
+end
+
 
 ngx.req.get_body_data()
 
-ret.headers = ngx.req.get_headers()
-ret.get = ngx.req.get_uri_args()
-ret.post = ngx.req.get_post_args()
-ret.file = ngx.req.get_body_file()
+ret.headers = try(lib.get_headers)
+ret.get = try(lib.get_uri)
+ret.post = try(lib.get_body)
+ret.file = try(lib.get_file)
+
 
 local ok, error = redis:set(ngx.var.remote_addr, json.encode(ret))
 
