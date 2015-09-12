264f61a35b544f37002d6a2dccdc4b4c77997b46
c475 <danjamesbond@gmail.com>
2015-09-12 13:49:58 -0700

    .

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 WD = $(readlink -f "$0")
 SPATH = $(dirname "$WD")
 CONFIGDIR = "/usr/local/openresty/nginx/config/"
-rm -f $CONFIGDIR
+rm -f "${CONFIGDIR}/nginx.conf"
 cp "${SPATH}/config/nginx.conf" $CONFIGDIR
