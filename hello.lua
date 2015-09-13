a65f3936c0b098f7e5a5a7630fe78721be56b0a4
c475 <danjamesbond@gmail.com>
2015-09-12 18:07:39 -0700

    .

--- hello.lua
+++ hello.lua
@@ -45,7 +45,7 @@ ret.file    = lib.try(lib.get_file)
 
 local exists = redis:get(ngx.var.remote_addr)
 
-if type(exists) ~= "userdata" then
+if type(exists) == "string" then
     prior_records = json.decode(exists)
     prior_records[#prior_records] = ret
     redis:set(ngx.var.remote_addr, json.encode(prior_records))
